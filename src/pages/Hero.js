dede4634a0be9f404fbc52ce0f33a9ac599f27e7
Narayan Aryal <aryal.coding@gmail.com>
2023-07-06 23:51:38 +1000

    first page completed

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,31 +1,47 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
+import uber from "../assets/uber.jpg";
 
 export const Hero = () => {
   return (
     <div>
       <div className="hero">
-        <div className="info">
-          <Row className="p-2">
-            <Col className="">
+        <div className="info bg-light">
+          <Row className="p-5 ">
+            <Col className="flex-col text-center h5 curser">
               <p>
                 <i class="fa-solid fa-chart-simple"></i>
               </p>
               <p>Drive or Deliver</p>
             </Col>
-            <Col>
+            <Col className="flex-col text-center h5 curser">
               <p>
-                <i class="fa-solid fa-chart-simple"></i>
+                <i class="fa-solid fa-utensils"></i>
               </p>
-              <p>Drive or Deliver</p>
+              <p>Eat</p>
             </Col>
-            <Col className="">
+            <Col className="flex-col text-center h5 curser">
               <p>
-                <i class="fa-solid fa-chart-simple"></i>
+                <i class="fa-solid fa-car"></i>
               </p>
-              <p>Drive or Deliver</p>
+              <p>Ride</p>
             </Col>
+            <hr />
           </Row>
+          <div>
+            <div className="title">Get in the driver's</div>
+            <div className="title">seat and get paid</div>
+            <p className="mt-5 mx-5">
+              Drive on the platform with the largest network of active riders.
+            </p>
+          </div>
+          <Button variant="dark" className="mt-3 p-3 fw-bold mx-5">
+            Sign up to drive
+          </Button>
+
+          <div className="bottom-info curser">
+            Learn more about driving and delivering
+          </div>
         </div>
       </div>
     </div>
