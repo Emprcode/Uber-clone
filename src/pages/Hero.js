47e70ddb5af5eed318a2d0a6b54c2c89b835c2d2
Narayan Aryal <aryal.coding@gmail.com>
2023-07-09 12:18:12 +1000

    code updated

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Row, Col, Button, Container } from "react-bootstrap";
 import uber from "../assets/uber.jpg";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
 export const Hero = () => {
   return (
-    <div className="hero">
+    <div className="hero" data-aos="fade-up" data-aos-duration="1000">
       <Container>
         <div className="box">
           <div className="info">
